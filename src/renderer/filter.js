f789c5c3a261099813e7d1fd1554c30cf5f95ea7
Jasan Hernandez <jasan_azael@hotmail.es>
2020-03-27 13:49:05 -0600

    saving original image

--- src/renderer/filter.js
+++ src/renderer/filter.js
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import fs from 'fs.extra';
 
 export const applyFilter = (filter, currentImage) => {
   let imgObj = new Image();
@@ -12,6 +12,11 @@ export const applyFilter = (filter, currentImage) => {
 
 export const saveImage = (fileName, callback) => {
   let fileSrc = document.getElementById('image-displayed').src;
-  fileSrc = fileSrc.replace(/^data:([A-Za-z-+/]+);base64,/, '');
-  fs.writeFile(fileName, fileSrc, 'base64', callback);
+  if (fileSrc.indexOf(';base64,') !== -1) {
+    fileSrc = fileSrc.replace(/^data:([A-Za-z-+/]+);base64,/, '');
+    fs.writeFile(fileName, fileSrc, 'base64', callback);
+  } else {
+    fileSrc = fileSrc.replace('file:///', '');
+    fs.copy(fileSrc, fileName, callback);
+  }
 };
