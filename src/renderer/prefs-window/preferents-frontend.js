66e4ca26d2eb3d198b782c5e1bfeab3970f1f918
Jasan Hernandez <jasan_azael@hotmail.es>
2020-03-27 18:53:09 -0600

    encrypt and decrypt password

--- src/renderer/prefs-window/preferents-frontend.js
+++ src/renderer/prefs-window/preferents-frontend.js
@@ -1,18 +1,28 @@
 import { remote } from 'electron';
 import settings from 'electron-settings';
 import { ipcRenderer } from 'electron';
+import crypto from 'crypto';
 
 window.addEventListener('load', () => {
   cancelButton();
   saveButton();
+  console.log(settings.file());
 
   if (settings.has('cloudup.user')) {
     document.getElementById('cloudup-user').value = settings.get(
       'cloudup.user'
     );
-    document.getElementById('cloudup-passwd').value = settings.get(
-      'cloudup.passwd'
+  }
+  if (settings.has('cloudup.passwd')) {
+    const decipher = crypto.createDecipher('aes-128-cbc', 'mypassword');
+    let decryped = decipher.update(
+      settings.get('cloudup.passwd'),
+      'hex',
+      'utf8'
     );
+    decryped += decipher.final('utf-8');
+
+    document.getElementById('cloudup-passwd').value = decryped;
   }
 });
 
@@ -31,14 +41,19 @@ const saveButton = () => {
 
   $saveBtn.addEventListener('click', () => {
     if (prefsForm.reportValidity()) {
+      const cipher = crypto.createCipher('aes-128-cbc', 'mypassword');
+      let encrypted = cipher.update(
+        document.getElementById('cloudup-passwd').value,
+        'utf8',
+        'hex'
+      );
+      encrypted += cipher.final('hex');
+
       settings.set(
         'cloudup.user',
         document.getElementById('cloudup-user').value
       );
-      settings.set(
-        'cloudup.passwd',
-        document.getElementById('cloudup-passwd').value
-      );
+      settings.set('cloudup.passwd', encrypted);
       const prefsWindow = remote.getCurrentWindow();
       prefsWindow.close();
     } else {
